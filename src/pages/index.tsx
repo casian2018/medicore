94f3135d05beb57ef7d6cb496e2cf5d0d6ab286c
Chriss42096 <cristian.muntianu07@gmail.com>
2025-03-14 22:10:58 +0200

    chore: Update navigation styling and add footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Nav from '@/components/nav'
+import Footer from '@/components/footer'
 
 export default function Home() {
   return (
     <>
     <Nav />
-    
+    <div className='flex h-screen'>
+      <h1></h1>
+    </div>
+    <Footer/>
     </>
   );
 }
