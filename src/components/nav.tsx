a8d843a237bbfae435d77b31ff43c47ffb5918ea
andrei <chesa.andrei0@gmail.com>
2025-03-15 15:43:57 +0200

    Update nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -16,7 +16,7 @@ export default function Nav() {
 
           <a
             href="/pages/login"
-            className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none"
+            className="text-white bg-red-700 hover:bg-red-800 focus:ring-4font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 focus:outline-none"
           >
             Sign In
           </a>
