69253e265c53164677d5b3c83e959547974ed7bf
andrei <chesa.andrei0@gmail.com>
2025-03-16 01:02:16 +0200

    Update nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -99,7 +99,7 @@ const Nav = () => {
                         </li>
                         <li>
                             <Link
-                                href="/pages/talk"
+                                href="/talk"
                                 className="block py-2 pl-3 pr-4 text-black border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-700 lg:p-0"
                             >
                                 Diagnosis
