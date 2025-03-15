29bc019295d336d2d19ef8abd3c51ac74bb99831
andrei <chesa.andrei0@gmail.com>
2025-03-15 11:55:16 +0200

    .

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Nav() {
   return (
-    <nav className="bg-white border-gray-200 py-2.5 w-full">
+    <nav className="bg-white border-gray-200 py-2.5 fixed  w-full">
       <div className="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
         <Link href="/" className="flex items-center">
           <span className="self-center text-xl font-semibold whitespace-nowrap">
