abead72e7d56e03825573da6e2c737da7dc0fe49
Chriss42096 <cristian.muntianu07@gmail.com>
2025-03-15 13:37:48 +0200

    upd

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -75,7 +75,7 @@ export default function Nav() {
                 href="/pages/features"
                 className="block py-2 pl-3 pr-4 text-black border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-red-700 lg:p-0"
               >
-                Features
+                Diagnosis
               </Link>
             </li>
             <li>
